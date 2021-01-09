30700e0cc178595fe3533941110850990620f2ad
甲蛙 <test@imooc.com>
2021-01-09 15:06:02 +0800

    7-4 分类管理不显示分页

--- src/main/java/com/jiawa/wiki/controller/CategoryController.java
+++ src/main/java/com/jiawa/wiki/controller/CategoryController.java
@@ -2,14 +2,15 @@ package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.req.CategoryQueryReq;
 import com.jiawa.wiki.req.CategorySaveReq;
-import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.resp.CategoryQueryResp;
+import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.resp.PageResp;
 import com.jiawa.wiki.service.CategoryService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/category")
@@ -17,6 +18,14 @@ public class CategoryController {
 
     @Resource
     private CategoryService categoryService;
+
+    @GetMapping("/all")
+    public CommonResp all() {
+        CommonResp<List<CategoryQueryResp>> resp = new CommonResp<>();
+        List<CategoryQueryResp> list = categoryService.all();
+        resp.setContent(list);
+        return resp;
+    }
 
     @GetMapping("/list")
     public CommonResp list(@Valid CategoryQueryReq req) {
