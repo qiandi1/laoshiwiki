6cba18382d431d83bf035af3a3ba39f780ec53ce
甲蛙 <test@imooc.com>
2021-01-24 09:33:50 +0800

    8-11 增加读取文档内容功能

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -110,4 +110,9 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+
+    public String findContent(Long id) {
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }
