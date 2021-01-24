8913a1ca5fd68aab6cce1b838ad4da5c34b27ad6
甲蛙 <test@imooc.com>
2021-01-24 18:56:35 +0800

    8-12 Bug修复，打开编辑页面时，应该初始化父文档选择框，免去手动点新增

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -124,6 +124,9 @@
       param.value = {};
       const docs = ref();
       const loading = ref(false);
+      // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
+      const treeSelectData = ref();
+      treeSelectData.value = [];
 
       const columns = [
         {
@@ -169,6 +172,11 @@
             level1.value = [];
             level1.value = Tool.array2Tree(docs.value, 0);
             console.log("树形结构：", level1);
+
+            // 父文档下拉框初始化，相当于点击新增
+            treeSelectData.value = Tool.copy(level1.value);
+            // 为选择树添加一个"无"
+            treeSelectData.value.unshift({id: 0, name: '无'});
           } else {
             message.error(data.message);
           }
@@ -176,9 +184,6 @@
       };
 
       // -------- 表单 ---------
-      // 因为树选择组件的属性状态，会随当前编辑的节点而变化，所以单独声明一个响应式变量
-      const treeSelectData = ref();
-      treeSelectData.value = [];
       const doc = ref();
       doc.value = {};
       const modalVisible = ref(false);
