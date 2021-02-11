05c5aaf1fcc10ffec80b7bead8cead0b4a2cc472
甲蛙 <test@imooc.com>
2021-02-11 13:19:37 +0800

    9-9 前端显示登录昵称，遗留问题，刷新会没有

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,7 +21,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
     </a-menu>
@@ -55,9 +58,14 @@
   export default defineComponent({
     name: 'the-header',
     setup () {
+      // 登录后保存
+      const user = ref();
+      user.value = {};
+
+      // 用来登录
       const loginUser = ref({
         loginName: "test",
-        password: "test"
+        password: "test123"
       });
       const loginModalVisible = ref(false);
       const loginModalLoading = ref(false);
@@ -76,6 +84,7 @@
           if (data.success) {
             loginModalVisible.value = false;
             message.success("登录成功！");
+            user.value = data.content;
           } else {
             message.error(data.message);
           }
@@ -87,7 +96,8 @@
         loginModalLoading,
         showLoginModal,
         loginUser,
-        login
+        login,
+        user
       }
     }
   });
