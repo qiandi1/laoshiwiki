45fe712164c63603d3cb651c5e65f4af8ead23de
甲蛙 <test@imooc.com>
2021-02-11 10:20:01 +0800

    9-8 前后端调试成功，完成基本的登录功能

--- src/main/java/com/jiawa/wiki/service/UserService.java
+++ src/main/java/com/jiawa/wiki/service/UserService.java
@@ -124,7 +124,7 @@ public class UserService {
         if (ObjectUtils.isEmpty(userDb)) {
             // 用户名不存在
             LOG.info("用户名不存在, {}", req.getLoginName());
-            throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
         } else {
             if (userDb.getPassword().equals(req.getPassword())) {
                 // 登录成功
@@ -133,7 +133,7 @@ public class UserService {
             } else {
                 // 密码不对
                 LOG.info("密码不对, 输入密码：{}, 数据库密码：{}", req.getPassword(), userDb.getPassword());
-                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+                throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
             }
         }
     }
