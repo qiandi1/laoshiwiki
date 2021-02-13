57e3626016693ba97d96772a1ce77236f5fd708b
甲蛙 <test@imooc.com>
2021-02-13 20:52:08 +0800

    10-5 定时任务示例

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 // @ComponentScan({"com.jiawa", "com.test"})
 @ComponentScan("com.jiawa")
 @SpringBootApplication
 @MapperScan("com.jiawa.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
 
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
