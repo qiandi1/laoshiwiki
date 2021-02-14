6e4fb63fa18341eec468952fd482245e8737dad9
甲蛙 <test@imooc.com>
2021-02-14 09:16:20 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -149,4 +149,8 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+
+    public void updateEbookInfo() {
+        docMapperCust.updateEbookInfo();
+    }
 }
