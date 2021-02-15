676d2d1b1d5e5b4b0a62a8ffa23b8741b8271dc2
甲蛙 <test@imooc.com>
2021-02-15 10:39:40 +0800

    10-11 使用RocketMQ解耦点赞通知功能，发送和接收调试成功 rocketmq本地启动：   mqnamesrv.cmd   mqbroker.cmd -n 127.0.0.1:9876 autoCreateTopicEnable=true   使用autoCreateTopicEnable，让客户端可以自由创建topic

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import com.jiawa.wiki.util.CopyUtil;
 import com.jiawa.wiki.util.RedisUtil;
 import com.jiawa.wiki.util.RequestContext;
 import com.jiawa.wiki.util.SnowFlake;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -50,6 +51,9 @@ public class DocService {
 
     @Resource
     public WsService wsService;
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
 
     public List<DocQueryResp> all(Long ebookId) {
         DocExample docExample = new DocExample();
@@ -149,7 +153,7 @@ public class DocService {
         // docMapperCust.increaseVoteCount(id);
         // 远程IP+doc.id作为key，24小时内不能重复
         String ip = RequestContext.getRemoteAddr();
-        if (redisUtil.validateRepeat("DOC_VOTE_" + id + "_" + ip, 3600 * 24)) {
+        if (redisUtil.validateRepeat("DOC_VOTE_" + id + "_" + ip, 5000)) {
             docMapperCust.increaseVoteCount(id);
         } else {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
@@ -158,7 +162,8 @@ public class DocService {
         // 推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
         String logId = MDC.get("LOG_ID");
-        wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
+        // wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
+        rocketMQTemplate.convertAndSend("VOTE_TOPIC", "【" + docDb.getName() + "】被点赞！");
     }
 
     public void updateEbookInfo() {
