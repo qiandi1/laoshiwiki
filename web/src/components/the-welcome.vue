b2b579531a17aff824a613995896f27019470f0c
甲蛙 <test@imooc.com>
2021-02-16 20:04:11 +0800

    11-6 将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>欢迎使用甲蛙知识库1</h1>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'the-welcome',
+    setup () {
+
+      return {
+
+      }
+    }
+  });
+</script>
