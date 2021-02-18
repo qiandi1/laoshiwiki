b2373c5e0ccf1dc202fcc68e346c66298fbd0791
甲蛙 <test@imooc.com>
2021-02-18 20:12:04 +0800

    12-6 SpringBoot多环境配置，生产发布修改

--- doc/all.sql
+++ doc/all.sql
@@ -7,9 +7,9 @@ create table `ebook` (
   `category2_id` bigint comment '分类2',
   `description` varchar(200) comment '描述',
   `cover` varchar(200) comment '封面',
-  `doc_count` int comment '文档数',
-  `view_count` int comment '阅读数',
-  `vote_count` int comment '点赞数',
+  `doc_count` int not null default 0 comment '文档数',
+  `view_count` int not null default 0 comment '阅读数',
+  `vote_count` int not null default 0 comment '点赞数',
   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='电子书';
 
